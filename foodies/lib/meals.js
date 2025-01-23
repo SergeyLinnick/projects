bd2e21508b1fef1848747591b2cb06a9597fcdf5
Serhii <sergey.linnick@gentlecode.com>
2025-01-23 20:54:05 +0100

    P1: Rename slug folder to mealSlug, add not found page for meals

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -5,6 +5,10 @@ const db = sql("meals.db");
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  // throw new Error("Loading meals failed");
+  // throw new Error('Loading meals failed');
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
