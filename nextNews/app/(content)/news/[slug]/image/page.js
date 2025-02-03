01652f1bbf4373ba553d308a87ba6a192f9f5bce
Serhii <sergey.linnick@gentlecode.com>
2025-02-03 23:07:51 +0100

    P2: migrate Project to a local Database

--- nextNews/app/(content)/news/[slug]/image/page.js
+++ nextNews/app/(content)/news/[slug]/image/page.js
@@ -1,8 +1,6 @@
 import { notFound } from "next/navigation";
 
-import { DUMMY_NEWS } from "@/dummy-news";
-
-export default function ImagePage({ params }) {
+export default async function ImagePage({ params }) {
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
